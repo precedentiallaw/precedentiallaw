3e8f103d605193f5b090d78e2e4b30c63290b5aa
Precedential Law <abdulhomeae@gmail.com>
2025-07-07 06:40:47 +0400

    Create Civilcases.tsx

---
+++ src/pages/services/Civilcases.tsx
@@ -0,0 +1,9 @@
+// src/pages/services/CivilCases.tsx
+export default function CivilCases() {
+  return (
+    <div>
+      <h1>Civil Cases</h1>
+      <p>Content coming soon...</p>
+    </div>
+  );
+}
